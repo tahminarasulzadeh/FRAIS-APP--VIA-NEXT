423f47930748efe17950f48579632bce4249b8d6
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2025-03-04 22:06:20 +0400

    first commit

---
+++ app/components/Logo/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function Logo () {
+    return (
+        <Link href="/" className="w-[150px] p-2 h-full border-[3px] border-solid border-black">
+            <p className="text-center text-black text-[22px] font-thin tracking-widest leading-snug">FRAIS</p>
+        </Link>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,40 @@
 import Image from "next/image";
+import HomeImg from '../public/images/pexels-tara-winstead-6682999.jpg'
+import Favorites from "./components/Favrorites/pages";
+import ProbioticBars from "./components/ProbioticBars/pages";
+import MostPopular from "./MostPopular/page";
+import NaturallySimple from "./components/Naturally/page";
+import Blog from "./components/Blog/page";
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    // landing page
+    <div>
+      {/* home page */}
+      <div className="flex xl:pl-24 xl:gap-0 gap-[10px] flex-col-reverse xl:row xl:justify-start xl:relative">
+        <Image className="xl:max-w-[1000px] xl:max-h-[760px]" src={HomeImg} alt="home page image" />
+        <div className="flex  px-5 justify-start items-start flex-col gap-[10px] xl:top-[25%] xl:gap-[20px] xl:left-[50%] xl:absolute">
+          <p className="text-black text-[16px] font-thin tracking-widest leading-snug xl:text-[20px]">HANDCRAFTED ORGANIC SOAPS & CANDLES</p>
+          <p className="text-black  text-[32px] xl:text-[52px] font-thin tracking-widest leading-snug">JUST LIKE NATURE
+            <br /> INTENDED</p>
+          <div className="flex justify-start items-start ">
+            <button className="bg-[#5E5E4A] text-white w-[120px] py-2 text-[14px] font-thin tracking-widest leading-snug 
+            hover:bg-[#B9B4B0] hover:border-[#5E5E4A] hover:text-[#5E5E4A] hover:border-[1px] 
+             transition-all duration-300 ease-in-out">
+              Shop Now
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+     {/* Shop our favorites */}
+     <Favorites/>
+     <ProbioticBars/>
+     <MostPopular/>
+     <NaturallySimple/>
+    <Blog />
     </div>
+
   );
 }
