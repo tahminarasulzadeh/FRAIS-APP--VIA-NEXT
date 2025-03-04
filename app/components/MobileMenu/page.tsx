0f50f2e2a37c01b85db99168383ea53a2aba5db3
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2025-03-04 23:00:28 +0400

    fixing bugs in mobile menu component

--- app/components/MobileMenu/page.tsx
+++ app/components/MobileMenu/page.tsx
@@ -2,21 +2,17 @@ import LogIn from "../LogIn/page"
 import Navbar from "../Navbar/page"
 import MobileSearchInput from "../mobileSearchInput/page";
 
-
 // Define the type for the props
 interface MobileMenuProps {
     click: () => void; // The `click` prop is a function with no arguments and no return value
 }
 
-const MobileMenu: React.FC<MobileMenuProps> = ({ click }) => {
-
-
-    
+const MobileMenu: React.FC<MobileMenuProps> = ({ click }) => {    
     return (
         // Menu for mobile
         <div className="py-5 gap-[20px] flex flex-col">
             <LogIn />
-            <MobileSearchInput click = {click} />
+            <MobileSearchInput click={click} />
             <div onClick={click}>
                 <Navbar />
             </div>
