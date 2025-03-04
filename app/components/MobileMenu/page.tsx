a4c60da18df9ed6cd3e9a7614b7012fd0c35e194
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2025-03-05 00:03:09 +0400

    updating mobile menu

--- app/components/MobileMenu/page.tsx
+++ app/components/MobileMenu/page.tsx
@@ -1,11 +1,10 @@
+
 import LogIn from "../LogIn/page";
 import Navbar from "../Navbar/page";
 import MobileSearchInput from "../mobileSearchInput/page";
 
-
- export default function MobileMenu({ click } : {click : () => void}) {
+export default function MobileMenu({ click }: { click: () => void }) {
     return (
-        // Menu for mobile
         <div className="py-5 gap-[20px] flex flex-col">
             <LogIn />
             <MobileSearchInput click={click} />
@@ -15,4 +14,5 @@ import MobileSearchInput from "../mobileSearchInput/page";
         </div>
     );
 }
+
 
